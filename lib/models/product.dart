364ab43a91b6fcf592d307d7c04ac84b0c1ade40
Peter <petermounir97@gmail.com>
2021-02-14 20:49:29 +0200

    Created the product model

---
+++ lib/models/product.dart
@@ -0,0 +1,50 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Product{
+  static const String kBrand = 'brand';
+  static const String kCategory = 'category';
+  static const String kFeatured = 'featured';
+  static const String kImages = 'images';
+  static const String kName = 'name';
+  static const String konSale = 'on sale';
+  static const String kPrice = 'price';
+  static const String kQuantity = 'quantity';
+  static const String kSizes = 'sizes';
+  static const String kId = 'id';
+
+  String _brand;
+  String _category;
+  bool _featured;
+  bool _onSale;
+  List<String> _images;
+  List<String> _sizes;
+  int _quantity;
+  double _price;
+  String _name;
+  String _id;
+
+  String get brand => _brand;
+  String get category => _category;
+  bool get featured => _featured;
+  bool get onSale => _onSale;
+  List<String> get images => _images;
+  List<String> get sizes => _sizes;
+  int get quantity => _quantity;
+  double get price => _price;
+  String get name => _name;
+  String get id => _id;
+
+  Product.fromSnapshot(DocumentSnapshot snapshot){
+    _featured = snapshot.data()[kFeatured];
+    _brand = snapshot.data()[kBrand];
+    _category = snapshot.data()[kCategory];
+    _onSale = snapshot.data()[konSale];
+    _images = snapshot.data()[kImages];
+    _sizes = snapshot.data()[kSizes];
+    _quantity = snapshot.data()[kQuantity];
+    _price = snapshot.data()[kPrice];
+    _name = snapshot.data()[kName];
+    _id = snapshot.data()[kId];
+  }
+
+}
